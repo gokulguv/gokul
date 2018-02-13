a0e66c34b75f87fd88b40761155dcc482dacfdaf
gokulguv <35332100+gokulguv@users.noreply.github.com>
2018-02-13 14:37:19 +0530

    Create smtwonm.c

---
+++ smtwonm.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	int a,b,c;
+	scanf("%d",&a);
+              printf("the first value%d",a);
+	scanf("%d",&b);
+	printf("the second value%d",b);
+	c=a+b;
+	printf("print the total value of%d",c);
+	
+	return 0;
+}
