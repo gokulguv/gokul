42401426f1594d5e9b3e441d9123e3f3308a81fc
gokulguv <35332100+gokulguv@users.noreply.github.com>
2018-02-22 10:39:02 +0000

    Create numinwrd.c

---
+++ numinwrd.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+int n;
+scanf("%d",&n);
+printf("enter the number");
+switch(n)
+{
+case 1:
+printf("one");
+break;
+case 2:
+printf("two");
+break;
+default:
+printf("\nnum not matched");
+}
+	return 0;
+}
