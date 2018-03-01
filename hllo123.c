9ae9fac946dcdb57194abb3f4f4fbb439c747d5e
gokulguv <35332100+gokulguv@users.noreply.github.com>
2018-03-01 10:50:16 +0000

    Create hllo123.c

---
+++ hllo123.c
@@ -0,0 +1,15 @@
+ #include <stdio.h>
+ int main() 
+{
+    char a[10];
+    int i;
+    gets(a);
+    for(i=0;i<1;i++)
+    {
+        if(a[i]>='1'&& a[i]<='9'||a[i]>='a'&& a[i]<='z')
+    { printf("yes");
+    }else
+    {printf("no");}
+    }
+    return 0;
+}
