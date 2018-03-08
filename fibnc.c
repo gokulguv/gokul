c345eef171bf6a5f91ac079b215d53e4e7863760
gokulguv <35332100+gokulguv@users.noreply.github.com>
2018-03-08 11:09:00 +0000

    Create fibnc.c

---
+++ fibnc.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int sum=0,a=1,n,i,s=0;
+	 printf("enter the series number");
+	 scanf("%d",&n);
+	 printf("%d%d",a,s);
+	 for(i=o;i<n;i++)
+	 {
+	 	
+	 	
+	 	
+	 	sum=s+a;
+	 	s=a;
+	 	a=sum;
+	 	
+	 	
+	 }
+	 printf("\n%d",sum);
+	 return 0;
+}
