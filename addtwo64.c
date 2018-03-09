5e7bf0709f4a20b700fbbc68fe72cda8aef84453
gokulguv <35332100+gokulguv@users.noreply.github.com>
2018-03-09 16:00:37 +0530

    Create addtwo64.c

---
+++ addtwo64.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("print the number");
+scanf("%d%d",&a,&b);
+c=a+b;
+if(c%=2)
+{
+	printf("odd");
+
+}
+else
+{
+	printf("even");
+	
+}
+
+return 0;
+}
