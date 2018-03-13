cddec18d2d1f141f13826a67a6e81fd85827b731
gokulguv <35332100+gokulguv@users.noreply.github.com>
2018-03-13 16:26:30 +0530

    Create prim66.c

---
+++ prim66.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int n,i,sum=0;
+scanf("%d",&n);
+
+for(i=1;i<=n;i++)
+{
+if(n%i==0)
+{
+sum++;
+}
+}
+if(sum==2)
+printf("yes");
+else
+printf("no");
+return 0;
+}
